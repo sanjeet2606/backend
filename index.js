46e451ee32480c85b38e8a79964c5849ab1c39b4
sanjeet kumar <kumarsanjeet966173@gmail.com>
2024-10-26 16:11:14 +0530

    new cors

--- index.js
+++ index.js
@@ -17,17 +17,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use((req, res, next) => {
-  const origin = 'https://frontend-eight-pi-98.vercel.app/';
-  if (process.env.allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin,X-Requested-With,Content-Type,Accept"
-  );
-  next();
-});
+// app.use((req, res, next) => {
+//   const origin = 'https://frontend-eight-pi-98.vercel.app/';
+//   if (process.env.allowedOrigins.includes(origin)) {
+//     res.setHeader("Access-Control-Allow-Origin", origin);
+//   }
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin,X-Requested-With,Content-Type,Accept"
+//   );
+//   next();
+// });
 // middleware
 // app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
